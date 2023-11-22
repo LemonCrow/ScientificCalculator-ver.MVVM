746fe001067d7a9bc94fff26223af18bf36be0c5
이준서 <lemon_crow@naver.com>
2023-11-22 17:59:31 +0900

    ViewModel 기초 작업 -inputNumber(입력 값) -currentExpression(수식)
    그 외 바인딩 작업

---
+++ ScientificCalculator ver.MVVM/ViewModels/ViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScientificCalculator_ver.MVVM.ViewModels
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private string inputNumber = "";
+        private string currentExpression = "";
+
+        public string InputNumber
+        {
+            get { return inputNumber; }
+            set { SetProperty(ref inputNumber, value); }
+        }
+
+        public string CurrentExpression
+        {
+            get { return currentExpression; }
+            set { SetProperty(ref currentExpression, value); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return;
+            field = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        // 추가적인 메소드 및 로직
+    }
+}
