fd45bb6e78dfeabe286b1e3cdd0792638bc68424
이준서 <lemon_crow@naver.com>
2023-11-22 16:30:10 +0900

    StartUpUri -> StartUP으로 변경 App.xaml 사유는 좀더 초기 로드를 세밀하게 제어하기 위해 미리 작업 및 폴더 배치 변경

--- ScientificCalculator ver.MVVM/App.xaml.cs
+++ ScientificCalculator ver.MVVM/App.xaml.cs
@@ -13,5 +13,10 @@ namespace ScientificCalculator_ver.MVVM
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow main = new();
+            main.ShowDialog();
+        }
     }
 }
